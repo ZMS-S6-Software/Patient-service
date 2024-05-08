d2904c249cf5e295c38d58f08b5de0ef1d13533f
Mattijs Pronk <456410@student.fontys.nl>
2024-05-08 10:12:23 +0200

    cors toegevoegd

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import cors from 'cors';
 //controllers
 import patientController from "./Controllers/PatientController.js";
 
@@ -10,6 +11,10 @@ dotenv.config();
 //app aanmaken
 const app = express();
 app.use(express.json());
+app.use(cors({
+  origin: process.env.FRONTEND || 'http://localhost:8083'
+}));
+
 
 //debug aanzetten
 mongoose.set('debug', true);
